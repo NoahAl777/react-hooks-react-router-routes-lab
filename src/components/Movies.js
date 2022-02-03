d2814466a83fcf210fda10309dd8e3374edb40de
Noah <crashlive112@gmail.com>
2022-02-03 14:44:56 -0500

    commit tree a122f1857e029556c799c6c57ba1944407360bc9

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => {
+        return (
+          <div key={index}>
+            <h2>{movie.title}</h2>
+            <h4>Time: {movie.time}</h4>
+            <h4>Genres: </h4>
+            <ul>
+              {movie.genres.map((genre, index) => <li key={index}>{genre}</li>)}
+            </ul>
+          </div>
+        )
+      })}
+    </>
+  );
 }
 
 export default Movies;
